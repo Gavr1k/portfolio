66e230cc0edbc67dcefc9b613c659c29c11aa4a2
Andrey Hauryk <andrey.hauryk@gmail.com>
2024-10-06 15:30:21 +0300

    add eslint to project

---
+++ src/components/widgets/Leetcode.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="widget"></div>
+</template>
+
+
+<style lang="ts"></style>
+
+<style scoped lang="scss">
+
+.widget {
+    background: #fff;
+    border-radius: 2rem;
+}
+
+</style>
