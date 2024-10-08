02f3bfb04b5645c02a6399700c18794770424c37
Andrey Hauryk <andrey.hauryk@gmail.com>
2024-10-08 22:47:36 +0300

    create linear progress

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,49 @@
 <template>
   <div>
     <ProgressCircular></ProgressCircular>
+    <div class="linear__progress" v-for="question in user.questions">
+      <LinearProgress 
+          :color="question.color" 
+          :value="question.resolved" 
+          :maxValue="question.quantity">
+      </LinearProgress>
+  </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import ProgressCircular from "./components/ProgressCircular.vue";
+import LinearProgress from "./components/LinearProgress.vue";
+import { ref } from 'vue';
+
+const user = ref({
+    name: 'Andrey Hauryk',
+    position: '#53567',
+    questions: [
+        {
+            id: 1,
+            name: 'Easy',
+            quantity: 812,
+            resolved: 81,
+            color: 'green',
+        },
+        {
+            id: 2,
+            name: 'Medium',
+            quantity: 1731,
+            resolved: 90,
+            color: 'orange',
+        },
+        {
+            id: 3,
+            name: 'Easy',
+            quantity: 749,
+            resolved: 17,
+            color: 'red',
+        },
+    ]
+});
+
 </script>
 
 
