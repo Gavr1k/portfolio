2c3c31c12a280fdb47c8b1af01bd2289e68474af
Andrey Hauryk <andrey.hauryk@gmail.com>
2024-10-10 21:17:58 +0300

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,74 @@
 <template>
   <div>
-    <LeetcodeWidget></LeetcodeWidget>
+    <Layout></Layout>
+    <div class="color_1 block"></div>
+    <div class="color_2 block"></div>
+    <div class="color_3 block"></div>
+    <div class="color_4 block"></div>
+    <div class="color_5 block"></div>
+    <div class="color_6 block"></div>
+
+    <!-- <LeetcodeWidget></LeetcodeWidget> -->
   </div>
 </template>
 
 <script setup lang="ts">
 import LeetcodeWidget from './components/LeetcodeWidget.vue';
+import Layout from './components/Layout.vue';
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+
+.block {
+    width: 300px;
+    height: 300px;
+}
+
+/*
+
+.color{
+    &_1 {
+        background-color: #0f1d40; //темно синий
+    }
+    &_2 {
+        background-color: #101e41; //насыщенный индиго
+    }
+    &_3 {
+        background-color: #101e41; //очень тёмный серо-синий
+    }
+    &_4 {
+        background-color: #102043; //глубокий сине-зелёный
+    }
+    &_5 {
+        background-color: #172549; //насыщенный синий
+    }
+    &_6 {
+        background-color: #162752 ; //тёмный индиго-синий
+    }
+}
+
+*/
+
+.color {
+    &_1 {
+        background-color: #0c0e0e; // тёмно-серый, почти чёрный
+    }
+    &_2 {
+        background-color: #edd6d8; // светло-розовый или бежевый
+    }
+    &_3 {
+        background-color: #0b0d0d; // очень тёмный серо-зелёный
+    }
+    &_4 {
+        background-color: #0d0f0f; // насыщенный тёмный серый
+    }
+    &_5 {
+        background-color: #141718; // чёрно-серый
+    }
+    &_6 {
+        background-color: #0a0c0c; // глубокий серо-чёрный
+    }
+}
+
+
+</style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="main-content">
+  <div class="concept concept-eight">
+    <h1 class="word">
+        <div class="char" data-content="A">A</div>
+        <div class="char" data-content="N">N</div>
+        <div class="char" data-content="D">D</div>
+        <div class="char" data-content="R">R</div>
+        <div class="char" data-content="E">E</div>
+        <div class="char" data-content="Y">Y</div>
+    </h1>
+  </div>
+</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+body {
+	background: #fff;
+	font-family: "Comfortaa", sans-serif;
+	position: relative;
+}
+
+.main-content {
+	text-align: center;
+	text-transform: uppercase;
+	scroll-snap-type: y mandatory;
+	position: relative;
+	height: 100vh;
+	overflow-y: scroll;
+}
+
+.hover,
+.word,
+h1 {
+	cursor: pointer;
+}
+
+h1 {
+	position: relative;
+	color: #fff;
+	font: 900 60px Montserrat;
+	text-shadow: 0 10px 25px rgba(0, 0, 0, 0.3);
+}
+
+.concept {
+	position: relative;
+	padding: 5em;
+	height: 100vh;
+	overflow: hidden;
+	scroll-snap-align: center;
+	&:before {
+		content: "";
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		background: radial-gradient(rgba(0, 0, 0, 0.3), transparent);
+		opacity: 0;
+		transition: all 1s cubic-bezier(0.19, 1, 0.22, 1);
+	}
+	&:hover:before {
+		opacity: 0.5;
+	}
+}
+
+.concept-eight {
+	background: url(../assets/header.jpg) top
+		right / cover;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	.word {
+		display: flex;
+		align-items: center;
+		justify-items: center;
+		width: 450px;
+		height: 80%;
+		&:hover .char:before {
+			transition: 0.6s ease;
+			color: rgba(255, 255, 255, 0.8);
+			animation: falls 1s ease forwards;
+		}
+		&:hover .char:nth-child(1):before {
+			animation: falls 0.9s ease 0.2s forwards;
+		}
+		&:hover .char:nth-child(2n + 3):before {
+			animation: falls 1s ease 0.3s forwards;
+		}
+	}
+	.char {
+		flex: 1;
+		position: relative;
+		&:before {
+			content: attr(data-content);
+			position: absolute;
+			top: 0;
+			color: transparent;
+			text-shadow: none;
+		}
+	}
+}
+
+@keyframes falls {
+	0% {
+		opacity: 0;
+		top: 0;
+	}
+	20% {
+		opacity: 1;
+	}
+	100% {
+		opacity: 0;
+		top: 100px;
+	}
+}
+
+</style>
